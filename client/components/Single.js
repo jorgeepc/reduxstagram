3e6f68d4b785eed03722f1f316c905bc05eb035c
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2017-09-04 17:37:20 -0500

    Initial commit

---
+++ client/components/Single.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
+class Single extends React.Component {
+  render() {
+    const { postId } = this.props.params;
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get the post
+    const post = this.props.posts[i];
+    // find the comments
+    const postComments = this.props.comments[postId] || [];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
+      </div>
+    )
+  }
+}
+
+export default Single;
